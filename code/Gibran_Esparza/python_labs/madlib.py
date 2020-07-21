38bf3db111e85dcb60eafb15f86b9a609605e19f
Vanessa Esparza <vanessaesparza@Vanessas-iMac.local>
2020-07-20 21:08:29 -0700

    added lab02_madlibs

---
+++ code/Gibran_Esparza/python_labs/madlib.py
@@ -0,0 +1,24 @@
+adjective_1 = input('Give me an Adjective: ')
+nationality= input('Give me a nationality: ')
+person= input('Give me a person: ')
+adjective_2= input('Give me an Adjective: ')
+noun_1= input('Give me a noun: ')
+noun_2= input('Give me a noun: ')
+adjective_3= input('Give me an Adjective: ')
+adjective_4= input('Give me an Adjective: ')
+plural_noun= input('Give me a plural noun: ')
+noun_3= input('Give me a noun: ')
+number= input('Give me a number: ')
+shapes= input('Give me a shape: ')
+food= input('Give me a food: ')
+food_2= input('Give me a food: ')
+number_2= input('Give me a number: ')
+
+messsage = f''' Pizza was invented by a {adjective_1} {nationality} chef named {person}
+To make pizza, you need to take a lump of {noun_1}, and make a thin, round {adjective_2}
+{noun_2}. Then you cover it with {adjective_3} sauce, {adjective_4} cheese, and fresh chopped
+{plural_noun}. Next you have to bake it in a very hot {noun_3}. When it is done, cut it into {number}
+{shapes}. Some kids like {food} pizza the best, but my favorite is the {food_2} pizza. If I could, I
+would eat pizza {number_2}times a day!'''
+
+print(messsage)
