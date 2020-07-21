0814069ab3022cda3f69eb9cc4b5711fea88fcfa
BuildTools <unconfigured@null.spigotmc.org>
2020-07-20 23:05:35 -0700

    'finally got the madlibs code working an repeating if incredibly ugly'

--- code/hugh/Python/Madlibs.py
+++ code/hugh/Python/Madlibs.py
@@ -2,13 +2,22 @@
 # first import the random module
 import random
 # initialize your user inpout list for the madlib to use
-userlist = [print(f'Please enter a noun: {input()}'),
-print(f'Please enter a pronoun: {input()}'),
-print(f'Please enter an adverb: {input()}'),
-print(f'Please enter a verb: {input()}')
-]
-while print(f'Hello and welcome to madlibs! Please follow the instructions or enter done at any time to leave: {input()}') != 'done':
+userlist = []
+
+print('Hello and welcome to madlibs! Please follow the instructions or enter done at any time to leave:')
+while input('Hit enter to continue, done to exit') != 'done':
+    userlist = [
+            input('Please enter a noun: '),
+            input('Please enter a pronoun: '),
+            input('Please enter an adverb: '),
+            input('Please enter a verb: ')
+            ]
     print(f'Hello TotallyNotEvilCorp is looking for a {random.choice(userlist)} evasion inspector. Must be willing to relocation to {random.choice(userlist)} for work or pay for travel expenses out of pocket. Please be aware any aversion to {random.choice(userlist)} will result in extreme discomfort but no additional job aids.')
-    while print(input('Type done to leave, update to change your words, and enter to shake up the madlib')) != 'done':
-        for words in userlist:
-            words = input()
+    while input('Play again? ') != 'done':
+        userlist = [
+            input('Please enter a noun: '),
+            input('Please enter a pronoun: '),
+            input('Please enter an adverb: '),
+            input('Please enter a verb: ')
+            ]
+        print(f'Hello TotallyNotEvilCorp is looking for a {random.choice(userlist)} evasion inspector. Must be willing to relocation to {random.choice(userlist)} for work or pay for travel expenses out of pocket. Please be aware any aversion to {random.choice(userlist)} will result in extreme discomfort but no additional job aids.')
