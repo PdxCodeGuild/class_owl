1e754b5b0a5e28888fb764c63d0527c92f25cc4e
Tannerisms <tanner.martin0313@gmail.com>
2020-07-23 22:38:55 -0500

    lab06

---
+++ code/Tanner/python/labs/date/20200721/practice01.py
@@ -0,0 +1,10 @@
+def prettyPrint(message = "hello world"):
+    print(message)
+    return True
+
+    carMakes = ['bmw', 'audi', 'VW', 'Datsun']
+    result = carMakes.lower()
+    print(result)
+prettyPrint()
+
+
