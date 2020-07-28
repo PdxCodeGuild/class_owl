69d107f14077535242faf3496286f51333901f0d
BuildTools <unconfigured@null.spigotmc.org>
2020-07-27 19:57:20 -0700

    'committing lab ten and old labs'

---
+++ code/hugh/Python/randoemo.py
@@ -0,0 +1,21 @@
+'''
+Let's generate emoticons by assembling a randomly choosing a set of eyes, a nose, and a mouth. Examples of emoticons are :-D =^P and :\
+
+Define a list of eyes
+Define a list of noses
+Define a list of mouths
+Randomly pick a set of eyes
+Randomly pick a nose
+Randomly pick a mouth
+Assemble and display the emoticon
+import random
+
+fruits = ['apple', 'banana', 'pineapple']
+fruit = random.choice(fruits)
+print(fruit)
+Example output:
+
+:-P
+Version 2
+Use a while loop to generate 5 emoticons.
+'''
