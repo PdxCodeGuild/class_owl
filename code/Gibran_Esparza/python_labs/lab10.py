51dace8b46ccd6cdf84955ee312a80a52256fbc6
Vanessa Esparza <vanessaesparza@Vanessas-iMac.local>
2020-07-27 20:35:45 -0700

    adding lab10

---
+++ code/Gibran_Esparza/python_labs/lab10.py
@@ -0,0 +1,46 @@
+#Lab10 Average Numbers
+
+#list of numbers
+nums = [5,0,8,3,4,1,6]
+#sum prior to looping and adding a total 
+sumtotal = 0
+#looping through each element in list and keeping a running total
+for num in nums:
+    sumtotal += num
+#Dividing our Sum by the lenght of our list  
+result = (sumtotal/ len(nums)).__round__(2)
+
+print(result)
+
+#Version 2
+
+#Setting an Empty List
+numlist =[]
+#Looping 
+while True:
+    #Getting User input and appending to list
+    
+    number = input("enter a number or 'done' to calculate average: ")
+    if number != 'done':
+        try:
+           number = int(number) 
+        except ValueError:
+            print("Enter a valid message")
+        else:
+            numlist.append(number)
+    #Breaking loop and Calculating Average
+    elif number == 'done':
+        total = 0
+        for num in numlist:
+            total += num
+        print(f"The average of numbers is {total/len(numlist)")
+        break
+       
+    
+   
+       
+
+
+
+
+                
