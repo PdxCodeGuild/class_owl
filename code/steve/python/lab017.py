a3de38d0cf3da15f75ffb804f294d7b435d89da1
diamondtron24 <stevecollins4@gmail.com>
2020-08-03 21:24:11 -0700

    lab017 commit 3

--- code/steve/python/lab017.py
+++ code/steve/python/lab017.py
@@ -19,8 +19,11 @@ def palindrome(word):
     
     return answer
 
-
-
+'''
+This function breaks down the 2 words into lowercase words, taking out any spaces
+then creating a list out of each word. Finally it sorts the word and compares them, 
+returning the answer.
+'''
 def breakdown(word_check1, word_check2):
     word_check_one = word_check1.lower().replace(' ', '')
     word_check_two = word_check2.lower().replace(' ', '')
@@ -37,15 +40,26 @@ def breakdown(word_check1, word_check2):
 
     return answer
 
+'''
+gets the first word to check 
+'''
 def user_input1():
     word_check1 = input("Please enter the word you\'d like to check as an anagram: ") 
     return word_check1
 
+
+'''
+gets the second word to check 
+'''
 def user_input2():
     word_check2 = input("Please enter the second word you\'d like to check as an anagram: ") 
     return word_check2
 
-
+'''
+This is the main function def. It asks for the word to compare for the anagram, and prints
+the answer. Then it leads into the palidrome check, calling functions to get the words,
+then printing whether it's a palindrome or not. 
+'''
 def main():
     word = input('Please enter a word to check if it\'s a palindrome: ')
     answer1 = palindrome(word)
