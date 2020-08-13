74639028665f92a5dcb620fc1654d7bb0865893a
Tannerisms <tanner.martin0313@gmail.com>
2020-08-12 22:50:15 -0500

    pract3

---
+++ code/Tanner/python/labs/date/20200812/practice3.py
@@ -0,0 +1,25 @@
+# def reverse(things):
+#     reversed_list = [things[i] for i in range(len(things) -1, -1, -1)]
+
+#     return things[::-1]
+
+
+# def new_list(nums):
+#     lessten = [num for num in nums if num < 10]
+#         return
+#     # for num in nums:
+#     #     if num < 10:
+#     #         lessten.append(num)
+
+# numbers = list(range(0,20))
+# random.shuffle(numbers)
+
+# def find_pairs(numbers, target):
+
+#     for i in range(len(numbers) -1):
+#         if numbers[i] + numbers[i + 1] == target:
+#             return [numbers[i], numbers[i + 1]]
+
+# print(find_pairs([1,2,3,4,5,6], 7))
+
+
