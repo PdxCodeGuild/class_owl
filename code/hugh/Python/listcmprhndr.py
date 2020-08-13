f68d1cf7520d1ce168b70fc49802c03d795d4163
BuildTools <unconfigured@null.spigotmc.org>
2020-08-12 19:13:59 -0700

    '''adding notes and touching code'''

---
+++ code/hugh/Python/listcmprhndr.py
@@ -0,0 +1,67 @@
+#List comprehensions allow us to generate a list with a single statement
+
+nums = []
+for x in range(10):
+    nums.append(x ** 2)
+print(nums)
+
+
+nums = [x ** 2 for x in range(10)]
+print(nums)
+
+words = '''a
+aa
+aaa
+aachen
+aardvark
+aardvarks
+aaron
+aback
+abacus
+abacuses
+abaft
+abalone
+abandon
+abandoned
+abandoning
+abandonment
+abandons
+abase
+abased
+abasement
+abases
+abash
+abashed
+abasing
+abate
+abated
+abatement
+abatements
+abates
+abating
+abattoir
+abattoirs
+abbess
+abbesses
+abbey
+abbeys
+abbot
+abbots
+abbreviate
+abbreviated
+abbreviates
+abbreviating
+abbreviation
+abbreviations
+abc'''
+
+words = words.split('\n')
+
+long_words = [word for word in words if len(word)>5]
+
+print(long_words)
+
+numbers = '133425453'
+
+intnumbslist = [int(num) for num in numbers]
+print(intnumbslist)
