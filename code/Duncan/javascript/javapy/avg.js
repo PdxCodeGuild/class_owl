f7e36b066d44cafee5d3536832735b9080d8bae5
Duncan <destout1983@yahoo.com>
2020-09-10 20:08:18 -0700

    added finished avg lab

--- code/Duncan/javascript/javapy/avg.js
+++ code/Duncan/javascript/javapy/avg.js
@@ -5,46 +5,74 @@ let thirdNum = document.getElementById("thirdNum")
 let fourthNum = document.getElementById("fourthNum")
 let fithNum = document.getElementById("fithNum")
 let output = document.getElementById("output")
+let calc = document.getElementById("calc")
+let body = document.getElementById("body")
+
 
 let nums = []
 
 firstNum.addEventListener("input", function(){
-    let num1 = nums.push(firstNum.value)
+    nums.push(parseInt(firstNum.value));
     console.log(nums[0])
 })
 
 secondNum.addEventListener("input", function(){
-    let num2 = nums.push(secondNum.value)
+    nums.push(parseInt(secondNum.value))
     console.log(nums)
 })
 
 thirdNum.addEventListener("input", function(){
-    let num3 = nums.push(thirdNum.value)
+    nums.push(parseInt(thirdNum.value))
     console.log(nums)
 })
 
 fourthNum.addEventListener("input", function(){
-    let num4 = nums.push(fourthNum.value)
+    nums.push(parseInt(fourthNum.value))
     console.log(nums)
 })
 
 fithNum.addEventListener("input", function(){
-    let num5 = nums.push(fithNum.value)
+    nums.push(parseInt(fithNum.value))
     console.log(nums)
 })
 
-output.addEventListener("click", function(){
+calc.addEventListener("click", function(){
+    var avg = nums.reduce((a, b) => a + b, );
     
-
+    output.innerText = `Your Average is ${avg}`
+    console.log(` the avg is : ${avg}. `)
 })
 
+// couldnt get this to work
+
+// fithNum.addEventListener("keyup",(function(event){
+//     if (event.keycode === 13) {
+//         console.log();
+//         calc.click();
+//     };
+
+//     // let click = function() {
+//     //     var avg = nums.reduce((a, b) => a + b, );
+    
+//     //     output.innerText = `Your Average is ${avg}`
+//     //     console.log(` the avg is : ${avg}. `) 
+//     // }
+// }))
 
 
-var array = [1, 2, 3, 4, 5];
+
+
+
+
+
+
+
+
+// var array = [1, 2, 3, 4, 5];
     
-    // Getting sum of numbers
-    var sum = array.reduce(function(a, b){
-        return a + b;
-    }, 0);
+//     // Getting sum of numbers
+//     var sum = array.reduce(function(a, b){
+//         return a + b;
+//     }, 0);
     
-    console.log(sum); // Prints: 15
+//     console.log(sum); // Prints: 15
