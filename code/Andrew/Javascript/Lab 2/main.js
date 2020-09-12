379e1e046a194c4b10de57be2f04647673a41a17
Tadashi-Croy <Tadashicroy@gmail.com>
2020-09-12 07:32:07 -0700

    Lab 1-2

--- code/Andrew/Javascript/Lab 2/main.js
+++ code/Andrew/Javascript/Lab 2/main.js
@@ -4,11 +4,11 @@ let result = document.querySelector("#resultPassword")
 let reIn1 = document.querySelector("#reInput1")
 let reIn2 = document.querySelector("#reInput2")
 let hider = document.getElementsByClassName("hide")
-
- 
+let verifyBtn = document.querySelector("#verify")
+let body = document.querySelector("#output") 
 
 btn.addEventListener("click", function() {
-    let password = ''
+    var password = ''
     if (passLen.value >0 && passLen.value <=16){
         
         for (let i =0; i<passLen.value; i++){
@@ -28,10 +28,34 @@ btn.addEventListener("click", function() {
     result.style.color = "red"
     result.style["font-size"] = "1.5rem"
 
-    hider.classList.remove("hiders")
+    for (i of hider){
+        console.log(i)
+        i.classList.remove("hiders")
+    }
+
+    
 
 })
 
+verifyBtn.addEventListener('click',function(){
+    console.log(result.innerText)
 
+    console.log(reIn1.value)
+    if(reIn1.value === reIn2.value && reIn2.value === result.innerText){
+        
+        let header = document.createElement("h1")
+        let hB = document.createTextNode("Password Verfied to work!")
+        header.appendChild(hB)
+        
+        body.appendChild(header)
+        header.style.color = "green"
+    }
+    else{
+        console.log(reIn1.value)
+        console.log(reIn2.value)
+        console.log(result.innerText)
+    }
+    
+})
 
 
