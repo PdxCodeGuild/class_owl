3b8a0fc9663527b5d8b070aee928b5b81f1208bb
Duncan <destout1983@yahoo.com>
2020-09-17 21:33:05 -0700

    added updated todo

--- code/Duncan/javascript/todo/main.js
+++ code/Duncan/javascript/todo/main.js
@@ -1,21 +1,62 @@
-let intro = document.querySelector("#intro")
-let firstI = document.querySelector("#firstI")
-let create = document.querySelector("#create")
-let usrInp = document.querySelector("#usrInp")
-let todo = document.querySelector(".todo")
+let form = document.getElementsByClassName("form")
+let newItem = document.getElementById("newItem")
+let add = document.getElementById("add")
+let ilist = document.getElementsByClassName("ilist")
+let delist = document.getElementsByClassName("delist")
+delist = delist[0]
+ilist = ilist[0]
+form = form[0]
 
 
-function Ilist() {
-    let newItem = document.createElement("input");
-    newItem.className = "todo";
-    document.getElementById("usrInp").appendChild(newItem)
+add.addEventListener("click", additem)
+
+function additem(){
+    console.log("hi")
+    let newitem = newItem.value 
+    
+    
+    let list = document.createElement("div")
+    let listitem = document.createElement("h3")
+    let done = document.createElement("button")
+    let del = document.createElement("button") 
+    
+    list.className = "list"
+    del.className = "delete"
+    done.className = "finished"
+    
+    listitem.innerText = `${newitem}`
+    list.appendChild(listitem)
+    
+    
+    list.appendChild(done)
+    list.appendChild(del)
+    ilist.appendChild(list)
+    done.innerText = "Finished"
+    
+    del.innerText = "DELETE"
+
+    done.addEventListener("click", finishedI)
+
+    function finishedI(){
+        console.log("donesers")
+        let dlist = document.createElement("div")
+        let dlistI = document.createElement("h3")
+        let del = document.createElement("button")
+
+        delist.appendChild("dlist")
+        dlist.appendChild("dlistI")
+        dlist.appendChild("del")
+    
+        dlistI.innerText = `${listitem}`
+    
+    
+    }
 }
-create.addEventListener("click", Ilist)
 
 
-function del() {
-    let delItem = usrInp.removeChild()
-}
+
+
+
 
 
 
