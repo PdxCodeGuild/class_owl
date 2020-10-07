bf78d694e8ab503684f95b3c4ff5426a194e2665
Duncan <destout1983@yahoo.com>
2020-10-06 21:19:06 -0700

    added updated calc

--- code/Duncan/javascript/vuecalc/main.js
+++ code/Duncan/javascript/vuecalc/main.js
@@ -1,6 +1,17 @@
 let app = new Vue({
     el: '#app',
     data: {
-
+        
+        numbers: ""
+    },
+    methods:{
+        
+        nums(a){
+            let button = document.querySelector("#button")
+            this.numbers += a 
+            
+           
+            console.log(numbers)
+        }
     },
 })
