3d1f7ec6abbcb824406270cf438121c658f40676
Esparza <Gibran.Esparza@pepsico.com>
2020-10-13 14:40:15 -0700

    lab12

---
+++ code/Gibran_Esparza/javascript_labs/lab12/main.js
@@ -0,0 +1,35 @@
+let app = new Vue({
+    el: "#app",
+    data:{
+        firstName: '',
+        lastName: '',
+        description:'',
+        subtitle:'',
+        facebook:'',
+        instagram:'',
+
+        
+
+    },
+    methods:{
+        getPlayer: async function(){
+            let url = `https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=${this.firstName}%20${this.lastName}`
+           
+           
+            let response = await axios.get(url)
+            console.log(response)
+        
+
+            app.description = response.data.player[0].strDescriptionEN
+            app.title = response.data.player[0].strPlayer
+            app.subtitle = response.data.player[0].strTeam
+            app.facebook = "https://" + response.data.player[0].strFacebook
+            app.instagram = "https://www." + response.data.player[0].strInstagram
+
+            this.firstName = ''
+            this.lastName = ''
+        }
+
+    }
+
+})
