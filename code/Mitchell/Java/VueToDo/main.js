f97286b58213d7a2717f221e1a114507a266c444
Mitchell Lovett <mitch.lovett@gmail.com>
2020-10-13 19:05:00 -0400

    m

--- code/Mitchell/Java/VueToDo/main.js
+++ code/Mitchell/Java/VueToDo/main.js
@@ -1,22 +1,39 @@
-let app = new Vue({
-    el: '#app',
+let prg = new Vue({
+
+    el: "#app",
     data: {
-        message: '',
-        TuDoList: [],
-        
+        total: 0
     },
     methods: {
-        addtask: function() {
-            let todo = this.message
-            this.TuDoList.push({
-                task: todo,
-                completed: false
-            })
-
+        addTask: function(){
+                let toDo = this.tasks
+                if( this.userInput.length >0 && this.userInput.length <=50){
+                    toDo.push(
+                        {userInput: [this.userInput, true]}
+                        )
+                        console.log(toDo)
+                        this.userInput =''
+                    }
+                else if( this.userInput.length >50) {
+                    alert('Keep it short')
+                    this.userInput = this.userInput.slice(0,52)
+                }
         },
-        deleteTask: function(i) {
-            this.TuDoList.splice(i, 1)
-
+        remove: function(i){
+            this.tasks.splice(i,1)
         },
-    }
+        removeItem: function(i){
+            this.completedTask.splice(i,1)
+        },
+        taskDone: function(i){
+            let a = this.tasks.splice(i,1)
+            console.log()
+            this.completedTask.push(a[0]['userInput'][0])        
+        },
+        omit: function(){
+            this.userInput = ''
+            this.tasks =  []
+            this.completedTask = []
+        },      
+    },
 })
