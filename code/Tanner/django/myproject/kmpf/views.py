3ad803e2f658cf009e9127571023e88563417b56
Tannerisms <tanner.martin0313@gmail.com>
2020-10-15 23:35:51 -0500

    labs

--- code/Tanner/django/myproject/kmpf/views.py
+++ code/Tanner/django/myproject/kmpf/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import BlogPost
 
 # Create your views here.
 def index(request):
+
+    blog_posts = BlogPost.objects.order_by('-likes')
+
     context = {
-        'message': 'apple bottom jeans'
+        'message': 'apple bottom jeans',
+        'blog_posts': blog_posts
     }
     return render(request, 'kmpf/index.html', context)
 
---
+++ code/Tanner/documents/main.js
@@ -0,0 +1,1 @@
+let click = addEventListener(click, "hello")
